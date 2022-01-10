a0e7a80c0ab631d6923397d08117a259b726cf07
Tiensp <17521129@gm.uit.edu.vn>
2022-01-10 17:41:15 +0700

    Add Read Data file

---
+++ HotelRecommenderSys.py
@@ -0,0 +1,6 @@
+
+
+
+class HotelRecommenderSys(object):
+    def __init__(self) -> None:
+        super().__init__()
--- ReadData.py
+++ ReadData.py
@@ -10,3 +10,15 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+# Get Data from firebase
+def getHotelsData():
+    hotels = list(db.collection(u'hotels').stream())
+
+    hotels_dict = list(map(lambda x: x.to_dict(), hotels))
+    df = pd.DataFrame(hotels_dict)
+
+    df['ratingCount'] = df['ratingList'].str.len()      # Create new feature Rating Count
+    # Drop unuseful feature
+    df.drop(columns=['thumbnail', 'img', 'detailInfo', 'ratingList', 'utilities'], axis=1, inplace=True)
+    #print(df.info())
+    return df
